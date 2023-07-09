ad1199737fd6fe51050a8d2ff6201564eb299012
GenericProgrammer1234 <133603260+GenericProgrammer1234@users.noreply.github.com>
2023-07-09 15:25:28 +0500

    Used match case, added While wrapper and help command
    Resolves #1, #2, and #3

--- jsonconnect.py
+++ jsonconnect.py
@@ -18,27 +18,38 @@ def format_json(data, prefix='', nest=1):
                 print(f"{prefix}{key} | {value}")
 
 def parse_condition(txt):
-    if txt[0] == "if":
-        try:
-            obj = txt[1]
-        except IndexError:
-            print("Object does not exist")
-            return
-        if len(txt) > 2 and txt[2] == "exists":
-            return os.path.isfile(f"{obj}.json")
-        return "invc"
-    return None
+        if txt[0] == "if":
+            try:
+                obj = txt[1]
+            except IndexError:
+                print("Object does not exist")
+                return
+            if len(txt) > 2 and txt[2] == "exists":
+                return os.path.isfile(f"{obj}.json")
+            return "invc"
+        return None
 
 def parse(txt):
     global selected
     parts = txt.split()
     command = parts[0]
-
-    if command == "create":
-        if len(parts) > 2 and parts[2] == "if":
-            condition = parts[2:]
-            res = parse_condition(condition)
-            if res is True:
+    match command:
+        case "create":
+            if len(parts) > 2 and parts[2] == "if":
+                condition = parts[2:]
+                res = parse_condition(condition)
+                if res is True:
+                    try:
+                        with open(f"{parts[1]}.json", "x") as file:
+                            file.write("{}")
+                        print(f"Database `{parts[1]}` created")
+                    except IndexError:
+                        print("No name for the database provided")
+                    except FileExistsError:
+                        print("Database already exists")
+                elif res == "invc":
+                    print("Invalid condition")
+            else:
                 try:
                     with open(f"{parts[1]}.json", "x") as file:
                         file.write("{}")
@@ -47,22 +58,21 @@ def parse(txt):
                     print("No name for the database provided")
                 except FileExistsError:
                     print("Database already exists")
-            elif res == "invc":
-                print("Invalid condition")
-        else:
-            try:
-                with open(f"{parts[1]}.json", "x") as file:
-                    file.write("{}")
-                print(f"Database `{parts[1]}` created")
-            except IndexError:
-                print("No name for the database provided")
-            except FileExistsError:
-                print("Database already exists")
-    elif command == "delete":
-        if len(parts) > 2 and parts[2] == "if":
-            condition = parts[2:]
-            res = parse_condition(condition)
-            if res is True:
+        case "delete":
+            if len(parts) > 2 and parts[2] == "if":
+                condition = parts[2:]
+                res = parse_condition(condition)
+                if res is True:
+                    try:
+                        os.remove(f"{parts[1]}.json")
+                        print(f"Database `{parts[1]}` deleted")
+                    except IndexError:
+                        print("Database doesn't exist")
+                    except FileNotFoundError:
+                        print("Database doesn't exist")
+                elif res == "invc":
+                    print("Invalid condition")
+            else:
                 try:
                     os.remove(f"{parts[1]}.json")
                     print(f"Database `{parts[1]}` deleted")
@@ -70,39 +80,47 @@ def parse(txt):
                     print("Database doesn't exist")
                 except FileNotFoundError:
                     print("Database doesn't exist")
-            elif res == "invc":
-                print("Invalid condition")
-        else:
-            try:
-                os.remove(f"{parts[1]}.json")
-                print(f"Database `{parts[1]}` deleted")
-            except IndexError:
-                print("Database doesn't exist")
-            except FileNotFoundError:
-                print("Database doesn't exist")
-    elif command == "select":
-        if len(parts) > 2 and parts[2] == "if":
-            condition = parts[2:]
-            res = parse_condition(condition)
-            if res is True:
+        case "select":
+            if len(parts) > 2 and parts[2] == "if":
+                condition = parts[2:]
+                res = parse_condition(condition)
+                if res is True:
+                    try:
+                        selected = parts[1]
+                        print(f"Database `{parts[1]}` selected")
+                    except IndexError:
+                        print("Database doesn't exist")
+                    except FileNotFoundError:
+                        print("Database doesn't exist")
+                elif res == "invc":
+                    print("Invalid condition")
+            else:
                 try:
                     selected = parts[1]
                     print(f"Database `{parts[1]}` selected")
                 except IndexError:
                     print("Database doesn't exist")
-            elif res == "invc":
-                print("Invalid condition")
-        else:
-            try:
-                selected = parts[1]
-                print(f"Database `{parts[1]}` selected")
-            except IndexError:
-                print("Database doesn't exist")
-    elif command == "insert":
-        if len(parts) > 2 and parts[2] == "if":
-            condition = parts[2:]
-            res = parse_condition(condition)
-            if res is True:
+                except FileNotFoundError:
+                    print("Database doesn't exist")
+        case "insert":
+            if len(parts) > 2 and parts[2] == "if":
+                condition = parts[2:]
+                res = parse_condition(condition)
+                if res is True:
+                    try:
+                        with open(f"{selected}.json", "r") as file:
+                            data = json.load(file)
+                        data[parts[1]] = parts[2]
+                        with open(f"{selected}.json", "w") as file:
+                            json.dump(data, file)
+                        print(f"Key `{parts[1]}` with the value `{parts[2]}` inserted")
+                    except IndexError:
+                        print("Key or value not provided")
+                    except FileNotFoundError:
+                        print("Selected file not found")
+                elif res == "invc":
+                    print("Invalid condition")
+            else:
                 try:
                     with open(f"{selected}.json", "r") as file:
                         data = json.load(file)
@@ -112,42 +130,47 @@ def parse(txt):
                     print(f"Key `{parts[1]}` with the value `{parts[2]}` inserted")
                 except IndexError:
                     print("Key or value not provided")
-            elif res == "invc":
-                print("Invalid condition")
-        else:
-            try:
-                with open(f"{selected}.json", "r") as file:
-                    data = json.load(file)
-                data[parts[1]] = parts[2]
-                with open(f"{selected}.json", "w") as file:
-                    json.dump(data, file)
-                print(f"Key `{parts[1]}` with the value `{parts[2]}` inserted")
-            except IndexError:
-                print("Key or value not provided")
-    elif command == "show":
-        if len(parts) > 2 and parts[2] == "if":
-            condition = parts[2:]
-            res = parse_condition(condition)
-            if res is True:
+                except FileNotFoundError:
+                    print("Selected file not found")
+        case "show":
+            if len(parts) > 2 and parts[2] == "if":
+                condition = parts[2:]
+                res = parse_condition(condition)
+                if res is True:
+                    try:
+                        with open(f"{selected}.json", "r") as file:
+                            data = json.load(file)
+                            format_json(data)
+                    except FileNotFoundError:
+                        print("Selected file not found")
+                elif res == "invc":
+                    print("Invalid condition")
+            else:
+                try:
                     with open(f"{selected}.json", "r") as file:
                         data = json.load(file)
-                        format_json(data)
-            elif res == "invc":
-                print("Invalid condition")
-        else:
-            with open(f"{selected}.json", "r") as file:
-                data = json.load(file)
-                format_json(data)      
-    elif command == "clear":
-        os.system("cls" if os.name == "nt" else "clear")
-    elif command == "quit":
-        exit()
-    elif command == "if":
-        print("true" if parse_condition(parts) else "false")
-    else:
-        print(f"Command doesn't exist: {command}")
+                        format_json(data)      
+                except FileNotFoundError:
+                    print("Selected file not found")
+        case "help":
+            print("See full documentation at https://github.com/GenericProgrammer1234/JSONConnect#readme")
+            print("create <name> - create a database")
+            print("delete <name> - delete a database")
+            print("select <name> - select a database")
+            print("insert <key> <value> - insert JSON into the selected database")
+            print("show - show the selected database")
+            print("help - this command")
+        case "clear":
+            os.system("cls" if os.name == "nt" else "clear")
+        case "quit":
+            exit()
+        case "if":
+            print("true" if parse_condition(parts) else "false")
+        case _:
+            print(f"Command doesn't exist: {command}")
 
-while True:
-    txt = input("> ")
-    if txt:
-        parse(txt)
+if __name__ == "__main__":
+    while True:
+        txt = input("> ")
+        if txt:
+            parse(txt)
